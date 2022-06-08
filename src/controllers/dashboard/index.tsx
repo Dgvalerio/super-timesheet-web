118136d2da78a4a8ccee4bbe3c4dd06e176a8c63
Davi <al.davigvalerio@gmail.com>
2022-06-08 18:27:30 -0300

    feat: add graphs and skeleton

--- src/controllers/dashboard/index.tsx
+++ src/controllers/dashboard/index.tsx
@@ -1,17 +1,89 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+
+import { useTheme } from '@mui/material';
+
+import { ChartData } from 'chart.js';
+import { differenceInBusinessDays } from 'date-fns';
+import { mix } from 'polished';
 
 interface ControllerReturn {
+  toWork: number;
+  worked: number;
+  data: ChartData<any, any, any>; // eslint-disable-line @typescript-eslint/no-explicit-any
   loading: boolean;
 }
 
+const calcToWork = (dailyWorkload: number): number => {
+  const now = new Date();
+  const [year, month] = now.toISOString().split('-');
+
+  const diff = differenceInBusinessDays(
+    new Date(+year, +month, 1),
+    new Date(+year, +month - 1, 1)
+  );
+
+  return diff * dailyWorkload;
+};
+
 const useDashboardController = (): ControllerReturn => {
+  const theme = useTheme();
+  const dailyWorkload = 8;
+  const [toWork, setToWork] = useState<number>(calcToWork(dailyWorkload));
+  const [worked, setWorked] = useState<number>(0);
   const [loading, setLoading] = useState(true);
+
+  const loadMonthHours = useCallback(async () => {
+    setLoading(true);
+
+    try {
+      setWorked(0);
+    } catch (e) {
+      toast.error('Falha ao carregar a carga horária mensal!');
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const data: ChartData<any, any, any> = {
+    labels: ['Horas não trabalhadas', 'Horas trabalhadas'],
+    datasets: [
+      {
+        label: 'Horas trabalhadas',
+        data: [toWork - worked, worked],
+        backgroundColor: [
+          mix(
+            0.8,
+            theme.palette.background.default,
+            theme.palette.primary.dark
+          ),
+          theme.palette.primary.dark,
+        ],
+        borderColor: [
+          mix(
+            0.8,
+            theme.palette.background.default,
+            theme.palette.primary.dark
+          ),
+          theme.palette.primary.dark,
+        ],
+        borderWidth: 0,
+      },
+    ],
+  };
 
   useEffect(() => {
     setLoading(false);
   }, []);
 
-  return { loading };
+  useEffect(() => {
+    setToWork(calcToWork(dailyWorkload));
+
+    void loadMonthHours();
+  }, [dailyWorkload, loadMonthHours]);
+
+  return { toWork, worked, data, loading };
 };
 
 export default useDashboardController;
--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -1,19 +1,48 @@
+import { Doughnut } from 'react-chartjs-2';
+
 import { NextPage } from 'next';
 
 import MainWrapper from '@/components/main-wrapper';
 import useController from '@/controllers/dashboard';
 import Styles from '@/styles/dashboard';
-import { Grid, Typography } from '@mui/material';
+import { Card, CardContent, Grid, Skeleton, Typography } from '@mui/material';
+
+import { Chart as ChartJS, ArcElement, Tooltip } from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip);
 
 const DashboardView: NextPage = () => {
-  const { loading } = useController();
+  const { toWork, worked, data, loading } = useController();
 
   return (
     <MainWrapper title={'Dashboard'} loading={loading}>
       <Styles.Container>
-        <Grid container spacing={2} justifyContent="center">
-          <Grid item xs={7}>
-            <Typography variant="h1">Dashboard</Typography>
+        <Grid container>
+          <Grid item xs={4}>
+            {loading ? (
+              <Skeleton width="100%" height={490} sx={{ transform: 'none' }} />
+            ) : (
+              <Card variant="outlined">
+                <CardContent style={{ paddingBottom: 8 }}>
+                  <Doughnut data={data} />
+                  <br />
+                  <Typography gutterBottom variant="h5" component="div">
+                    Horas trabalhadas
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    No mês atual devem ser trabalhadas <b>{toWork} horas</b>,
+                    das quais você já trabalhou <b>{worked} horas</b>.
+                  </Typography>
+                  <Typography
+                    variant="overline"
+                    color="text.secondary"
+                    sx={{ fontSize: 10 }}
+                  >
+                    Calculado com base nos dias úteis do mês
+                  </Typography>
+                </CardContent>
+              </Card>
+            )}
           </Grid>
         </Grid>
       </Styles.Container>
