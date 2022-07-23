4a38c2355270c1e471c292113360fb0b3841d7dc
Davi <al.davigvalerio@gmail.com>
2022-07-23 19:16:21 -0300

    style(input): change calendar-picker-indicator color correctly

--- src/components/input-field/index.tsx
+++ src/components/input-field/index.tsx
@@ -3,6 +3,15 @@ import { FC } from 'react';
 import { TextField } from '@mui/material';
 import { TextFieldProps } from '@mui/material/TextField/TextField';
 
+import styled from 'styled-components';
+
+const Input = styled(TextField)`
+  & > div > input::-webkit-calendar-picker-indicator {
+    filter: ${({ theme }) =>
+      theme.palette.mode === 'dark' ? 'invert(1)' : 'invert(0)'};
+  }
+`;
+
 export const InputField: FC<TextFieldProps> = ({ ...props }) => (
-  <TextField type="text" variant="outlined" required fullWidth {...props} />
+  <Input type="text" variant="outlined" required fullWidth {...props} />
 );
