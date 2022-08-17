dd7d2710bc3046c98b31f35908da9e70adcbdbf3
Davi <al.davigvalerio@gmail.com>
2022-08-17 12:38:10 -0300

    feat(add-with-github-repository): add repository pagination

--- components/appointment/create/with-github/repository/select/index.tsx
+++ components/appointment/create/with-github/repository/select/index.tsx
@@ -3,11 +3,16 @@ import React, { FC, useEffect, useState } from 'react';
 import RepositoryCard from '@/components/appointment/create/with-github/repository/card';
 import { getOrgRepositories } from '@/components/appointment/create/with-github/repository/controller';
 import SelectRepositorySkeleton from '@/components/appointment/create/with-github/repository/select/skeleton';
-import { repositoryColor } from '@/components/appointment/create/with-github/repository/style';
+import RepositoryStyles, {
+  repositoryColor,
+} from '@/components/appointment/create/with-github/repository/style';
 import Repository from '@/components/appointment/create/with-github/repository/types';
 import SectionTitle from '@/components/appointment/create/with-github/section-title';
 import SelectedCard from '@/components/appointment/create/with-github/selected-card';
 import { Collapse, Grid } from '@mui/material';
+import { PaginationProps } from '@mui/material/Pagination/Pagination';
+
+const { Pagination } = RepositoryStyles;
 
 const SelectRepository: FC<{
   selected: string | null;
@@ -15,6 +20,16 @@ const SelectRepository: FC<{
 }> = ({ selected, handleSelect }) => {
   const [loading, setLoading] = useState(true);
   const [repositories, setRepositories] = useState<Repository.List>([]);
+  const [page, setPage] = useState(1);
+
+  const perPage = 10;
+
+  const last = page * perPage;
+  const first = last - perPage;
+  const totalPages = Math.ceil(repositories.length / perPage);
+
+  const handlePaginate: PaginationProps['onChange'] = (_, value) =>
+    setPage(value);
 
   useEffect(() => {
     setLoading(true);
@@ -39,13 +54,24 @@ const SelectRepository: FC<{
           <SelectRepositorySkeleton />
         ) : (
           <Grid container spacing={2}>
-            {repositories.map((item) => (
+            {repositories.slice(first, last).map((item) => (
               <RepositoryCard
                 key={item.id}
                 repository={item}
                 handleClick={handleSelect}
               />
             ))}
+            <Grid
+              item
+              xs={12}
+              sx={{ display: 'flex', justifyContent: 'center' }}
+            >
+              <Pagination
+                count={totalPages}
+                page={page}
+                onChange={handlePaginate}
+              />
+            </Grid>
           </Grid>
         )}
       </Grid>
--- components/appointment/create/with-github/repository/style.ts
+++ components/appointment/create/with-github/repository/style.ts
@@ -1,9 +1,17 @@
+import styled from '@emotion/styled';
+import { Pagination as PaginationMUI } from '@mui/material';
 import { blue } from '@mui/material/colors';
 
 import { transparentize } from 'polished';
 
 export const repositoryColor = transparentize(0.3, blue['900']);
 
-const RepositoryStyles = {};
+const Pagination = styled(PaginationMUI)`
+  .Mui-selected.MuiPaginationItem-page {
+    background-color: ${repositoryColor};
+  }
+`;
+
+const RepositoryStyles = { Pagination };
 
 export default RepositoryStyles;
