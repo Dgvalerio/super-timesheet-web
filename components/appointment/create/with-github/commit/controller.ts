96eaff49a91e378c28707aac9321a3d7d0b9e75a
Davi <al.davigvalerio@gmail.com>
2022-08-25 01:44:42 -0300

    feat(add-with-github-commit): get all commits correctly

--- components/appointment/create/with-github/commit/controller.ts
+++ components/appointment/create/with-github/commit/controller.ts
@@ -1,17 +1,42 @@
 import octokit from '@/api/github';
 import Commit from '@/components/appointment/create/with-github/commit/types';
 
+// todo: get from authentication
+const author = 'dgvalerio';
+
 const getBranchCommits = async (
   repo: string,
   sha: string
 ): Promise<Commit.List> => {
-  const response = await octokit.request('GET /repos/{owner}/{repo}/commits', {
-    owner: 'lubysoftware',
-    repo,
-    sha,
-  });
+  const options = { owner: 'lubysoftware', author, repo, sha, per_page: 100 };
 
-  return response.data;
+  const response = await octokit.request(
+    'GET /repos/{owner}/{repo}/commits',
+    options
+  );
+
+  if (!response.headers.link) return response.data;
+
+  const search = /page=(\d+)>; rel="last"/gm;
+
+  const execArray = search.exec(response.headers.link);
+
+  if (!execArray) return response.data;
+
+  let list: Commit.List = [];
+
+  const pages = Number(execArray[1]);
+
+  for (let page = 1; page <= pages; page++) {
+    const response = await octokit.request(
+      'GET /repos/{owner}/{repo}/commits',
+      { ...options, page }
+    );
+
+    list = list.concat(response.data);
+  }
+
+  return list;
 };
 
 export default getBranchCommits;
