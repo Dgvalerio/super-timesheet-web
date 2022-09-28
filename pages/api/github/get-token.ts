1da7993e5c6f82f77757f950d740ba0186b55e8f
Davi <al.davigvalerio@gmail.com>
2022-09-28 11:37:39 -0300

    refactor(add-with-github): improve get-token types

--- pages/api/github/get-token.ts
+++ pages/api/github/get-token.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 export interface GetTokenRouteResponse {
   access_token: string;
   token_type: 'bearer';
-  scope: 'read:user,user:email';
+  scope: string;
 }
 
 const GetTokenRoute = async (
